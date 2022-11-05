5eae993b366196e9ef07c63809062336d9c53a3a
RasyadAkmal <akmal.rasyad2513@gmail.com>
2022-11-06 02:17:24 +0700

    Commit Rasyad

--- src/App.js
+++ src/App.js
@@ -4,29 +4,14 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import Counter from "./learn-useState/Counter";
-import Effect from "./learn-useEffect/Index";
-import Context from "./learn-useContext/Index";
-import New from "./new-page/Index";
+import Home from "./Home/Index";
 function App() {
   return (
     <Router>
       <div className="App-header">
-        <nav className="NavBar">
-          <ul className="ul">
-            <li className="li">
-              <Link to="/">Use State</Link>
-              <Link to="effect">Use Effect</Link>
-              <Link to="context">Use Context</Link>
-              <Link to="new">New Page</Link>
-            </li>
-          </ul>
-        </nav>
+        <nav className="NavBar"></nav>
         <Routes>
-          <Route path="/" exact element={<Counter />} />
-          <Route path="effect" exact element={<Effect />} />
-          <Route path="context" exact element={<Context />} />
-          <Route path="new" exact element={<New />} />
+          <Route path="/" exact element={<Home />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Home/Index.js
@@ -0,0 +1,47 @@
+import "./Index.css";
+import React from "react";
+import axios from "axios";
+
+export default class Home extends React.Component {
+  state = {
+    persons: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      const persons = res.data;
+      console.log(res.data);
+      this.setState({ persons });
+      console.log(this.state.persons);
+    });
+  }
+
+  //Bagian Function Delete
+
+  render() {
+    return (
+      <div>
+        <div className="title-container">
+          <h1>Tugas Modul 5</h1>
+          <h4>Kelompok 17</h4>
+        </div>
+        {this.state.persons.map((person) => {
+          return (
+            <div className="post-card" key={person.id}>
+              <h2 className="post-title">{person.name}</h2>
+              <p className="post-body">Email : {person.email}</p>
+              <div className="button">
+                <button
+                  className="delete-btn"
+                  onClick={() => this.deleteHandlerUser(person.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
