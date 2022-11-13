28fa1c1bf7e59a3ec3b72cfb22c457b4ceecc623
Nuzulitastwn <63113078+Nuzulitastwn@users.noreply.github.com>
2022-11-13 22:45:39 +0700

    Update Index.js

--- src/Home/Index.js
+++ src/Home/Index.js
@@ -15,8 +15,17 @@ export default class Home extends React.Component {
       console.log(this.state.persons);
     });
   }
-
-  //Bagian Function Delete Handler
+  
+deleteHandlerUser = (props) => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/` + props)
+      .then((res) => {
+        let arr = this.state.persons;
+        var idx = props - 1;
+        arr.splice(idx, 1);
+        this.setState([...arr]);
+      });
+  };
 
   render() {
     return (
